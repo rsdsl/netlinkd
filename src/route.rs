2ff9105da3fec92fbbd21b6ed843e30785b40b20
HimbeerserverDE <himbeerserverde@gmail.com>
2023-05-06 19:52:14 +0200

    add ipv6 route add support

--- src/route.rs
+++ src/route.rs
@@ -1,6 +1,6 @@
 use crate::error::{Error, Result};
 
-use std::net::Ipv4Addr;
+use std::net::{Ipv4Addr, Ipv6Addr};
 
 use futures_util::TryStreamExt;
 use netlink_packet_route::rtnl::RT_SCOPE_LINK;
@@ -40,4 +40,40 @@ async fn do_add4(dst: Ipv4Addr, prefix_len: u8, rtr: Option<Ipv4Addr>, link: Str
 
 pub fn add4(dst: Ipv4Addr, prefix_len: u8, rtr: Option<Ipv4Addr>, link: String) -> Result<()> {
     Runtime::new()?.block_on(do_add4(dst, prefix_len, rtr, link))
+}
+
+async fn do_add6(dst: Ipv6Addr, prefix_len: u8, rtr: Option<Ipv6Addr>, link: String) -> Result<()> {
+    let (conn, handle, _) = rtnetlink::new_connection()?;
+    tokio::spawn(conn);
+
+    let link = handle
+        .link()
+        .get()
+        .match_name(link.clone())
+        .execute()
+        .try_next()
+        .await?
+        .ok_or(Error::LinkNotFound(link))?;
+
+    let id = link.header.index;
+
+    let mut add = handle
+        .route()
+        .add()
+        .v6()
+        .destination_prefix(dst, prefix_len)
+        .output_interface(id);
+
+    if let Some(rtr) = rtr {
+        add = add.gateway(rtr);
+    } else {
+        add = add.scope(RT_SCOPE_LINK);
+    }
+
+    add.execute().await?;
+    Ok(())
+}
+
+pub fn add6(dst: Ipv6Addr, prefix_len: u8, rtr: Option<Ipv6Addr>, link: String) -> Result<()> {
+    Runtime::new()?.block_on(do_add6(dst, prefix_len, rtr, link))
 }
