8537cd92e53ab3915fc1a7488215ff2cd86f7285
HimbeerserverDE <himbeerserverde@gmail.com>
2023-10-15 11:20:57 +0200

    change type of SIOC constants to c_ulong

--- src/tunnel.rs
+++ src/tunnel.rs
@@ -1,13 +1,13 @@
 use crate::{Error, Result};
 
-use std::ffi::CString;
+use std::ffi::{c_ulong, CString};
 use std::io;
 use std::net::{Ipv4Addr, Ipv6Addr};
 
 use bitfield::bitfield;
 
-const SIOCADDTUNNEL: u64 = 0x89F0 + 1;
-const SIOCDELTUNNEL: u64 = 0x89F0 + 2;
+const SIOCADDTUNNEL: c_ulong = 0x89F0 + 1;
+const SIOCDELTUNNEL: c_ulong = 0x89F0 + 2;
 
 /// A handle to a 6in4 tunnel. The interface is automatically deleted on drop.
 #[derive(Debug)]
